34e24e9ef1378b0b59d9c91f7d366bef4670e3b8
Tanmay Haldar <iamtanmay2959@gmail.com>
2023-12-16 16:04:42 +0530

    im done

--- darft.js
+++ darft.js
@@ -1,22 +1,63 @@
 //? Problem 1
 
+function sumArray(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
 const sum = sumArray([1, 2, 3, 4, 5]);
 
 console.log(sum); //* Output: 15
 
+
+
 //? Problem 2
+
+function findMaxNumber(arr) {
+    let max = arr[0]; 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]; 
+        }
+    }
+    return max;
+}
 
 const maxNumber = findMaxNumber([10, 5, 8, 20, 3]);
 
 console.log(maxNumber); //* Output: 20
 
+
+
+
 //? Problem 3
+
+function filterOddNumbers(arr) {
+    let oddNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            oddNumbers[oddNumbers.length] = arr[i];
+        }
+    }
+    return oddNumbers;
+}
 
 const oddNumbers = filterOddNumbers([1, 2, 3, 4, 5]);
 
 console.log(oddNumbers); //* Output: [1, 3, 5]
 
+
+
+
 //? Problem 4
+
+function isPalindrome(str) {
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
 
 const isPalindromeResult = isPalindrome('radar');
 const isNonPalindromeResult = isPalindrome('hello');
@@ -26,11 +67,42 @@ console.log(isNonPalindromeResult); //* Output: false
 
 //? Problem 5
 
+function findMissingNumber(arr) {
+    let n = arr.length + 1;
+    let totalSum = 0;
+    let currentSum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        totalSum += i;
+    }
+
+    for (let num of arr) {
+        currentSum += num;
+    }
+
+    return totalSum - currentSum;
+}
+
 const missingNumber = findMissingNumber([1, 2, 3, 5, 6]);
 
 console.log(missingNumber); //* Output: 4
 
 //? Problem 6
+
+function findDuplicates(arr) {
+    const duplicates = [];
+    const count = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        count[num] = (count[num] || 0) + 1;
+        if (count[num] === 2) {
+            duplicates[duplicates.length] = num;
+        }
+    }
+
+    return duplicates;
+}
 
 const duplicateNumbers = findDuplicates([1, 2, 3, 2, 4, 5, 4, 6]);
 
--- main.js
+++ main.js
@@ -33,31 +33,83 @@ const flashcardsData = [
 
 const cardContainer = document.getElementById('card-container');
 const startButton = document.getElementById('start-button');
+const doneButton = document.getElementById('done-button');
 const resetButton = document.getElementById('reset-button');
+const scoreCard = document.getElementById('score-card');
+
+let score = 10;
+let wrong = 0;
+let clickedCards = [];
+
+const updateScore = () => {
+  score--;
+  wrong++;
+  scoreCard.innerText = "Score : " + score;
+};
 
 const handleStart = () => {
-  flashcardsData.forEach((item) => {
-    //* Card add section
+  clickedCards = [];
+  cardContainer.innerHTML = '';
+
+  flashcardsData.forEach((item, index) => {
     const cardElement = document.createElement('div');
     cardElement.className = 'border-2 border-green-500 rounded-md p-5 h-full';
     cardElement.innerText = item.word;
     cardContainer.appendChild(cardElement);
+    score = 10;
+    wrong = 0;
+    scoreCard.innerText = "Score : " + score;
 
-    //* Card click event
     cardElement.addEventListener('click', () => {
-      console.log(item.word);
       cardElement.classList.add('border-red-500');
       cardElement.innerText = item.meaning;
+      updateScore();
+      clickedCards.push({ index, card: item });
     });
   });
 
   resetButton.classList.remove('hidden');
   startButton.classList.add('hidden');
+  doneButton.classList.remove('hidden');
 };
 
 const handleReset = () => {
   cardContainer.innerHTML = '';
+  score = 10;
+  wrong = 0;
+  clickedCards = [];
 
   startButton.classList.remove('hidden');
   resetButton.classList.add('hidden');
+  doneButton.classList.add('hidden');
+  scoreCard.innerText = "";
 };
+
+const handleDone = () => {
+  scoreCard.innerText = "You got " + wrong + " words wrong. Score : " + score;
+
+  cardContainer.innerHTML = '';
+
+  clickedCards.sort((a, b) => a.index - b.index);
+
+  clickedCards.forEach(({ card }) => {
+    const cardElement = document.createElement('div');
+    cardElement.className = 'border-2 border-red-500 rounded-md p-5   h-full';
+
+    const wordElement = document.createElement('p');
+    wordElement.innerText = card.word;
+    wordElement.className = 'font-bold text-xl';
+    cardElement.appendChild(wordElement);
+
+    const meaningElement = document.createElement('p');
+    meaningElement.innerText = card.meaning;
+    cardElement.appendChild(meaningElement);
+
+    cardContainer.appendChild(cardElement);
+  });
+
+  resetButton.classList.add('hidden');
+  startButton.classList.remove('hidden');
+  doneButton.classList.add('hidden');
+};
+
